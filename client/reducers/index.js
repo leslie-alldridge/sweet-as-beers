3d3b9e277b3dfa438ec4da3015628552f79f0fcb
leslie.alldridge <leslie.alldridge@gmail.com>
2019-05-12 11:34:52 +1200

    redux

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import navReducer from "./reduce_nav";
 import cart from "./cart_reducer";
 import orders from "./orders_reducer";
+import auth from "./auth";
 
 export default combineReducers({
   navReducer,
   cart,
-  orders
+  orders,
+  auth
 });
